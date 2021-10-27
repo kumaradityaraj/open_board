69088c6944d6620ec625fc28821935082c300d1e
kumaradityaraj <sedulous.0007@gmail.com>
2021-10-27 19:41:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const socket = require("socket.io");
+
+let port =process.env.PORT ||3000;
+const app = express();
+
+app.use(express.static("public"));
+
+let server = app.listen(port, () => {
+  console.log("Listening to port " + port);
+});
+
+let io = socket(server);
+io.on("connection", (socket) => {
+  console.log("made socket connection");
+  socket.on("beginPath", (data) => {
+    io.sockets.emit("beginPath", data);
+  });
+  socket.on("drawStroke", (data) => {
+    io.sockets.emit("drawStroke", data);
+  });
+  socket.on("redoUndo", (data) => {
+    io.sockets.emit("redoUndo", data);
+  });
+});
