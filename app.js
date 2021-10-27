68554761913407f10f50c5d6f6f9156e1df1782e
kumaradityaraj <sedulous.0007@gmail.com>
2021-10-27 20:46:52 +0530

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const socket = require("socket.io");
 
-let port =process.env.PORT ||3000;
+let port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static("public"));
+
+app.use(require("cors")());
 
 let server = app.listen(port, () => {
   console.log("Listening to port " + port);
@@ -12,6 +14,10 @@ let server = app.listen(port, () => {
 
 let io = socket(server);
 io.on("connection", (socket) => {
+  cors: {
+    origin: "http://localhost:4200";
+    methods: ["GET", "POST"];
+  }
   console.log("made socket connection");
   socket.on("beginPath", (data) => {
     io.sockets.emit("beginPath", data);
